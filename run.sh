8d5c79448a0fadfb941975ff6d65a3da070fe665
c568254965 <568254965@qq.com>
2019-07-31 15:29:56 +0800

    .

--- run.sh
+++ run.sh
@@ -9,6 +9,6 @@ docker build -t user:test .
 docker run -d -p 6821:6821 --name user user:test java -Dspring.profiles.active=test -jar /app/app.jar
 
 
-# docker run -d -p 6821:6821 -v /code/huarui_res/user/log:/log  --label aliyun.logs.catalina=stdout  --label aliyun.logs.access=/code/huarui_res/user/log/.*.log    --name user user:test java -Dspring.profiles.active=test -jar /app/app.jar
+# docker run -d -p 6821:6821 -v /code/huarui_res/user/log:/log  --label aliyun.logs.catalina=stdout  --label aliyun.logs.access=/code/huarui_res/user/log/.*.log    --name user user:test java -Dspring.profiles.active=test -jar /app/app.jar > user.log
 
 # --label aliyun.logs.target=huarui_user_index
