5af890aa0569db1fcb15b325aa9d12169887f575
c568254965 <568254965@qq.com>
2019-07-31 14:20:57 +0800

    .

--- run.sh
+++ run.sh
@@ -6,4 +6,6 @@ mvn clean package -Dmaven.test.skip=true
 docker rm -f user
 docker rmi -f user:test
 docker build -t user:test .
-docker run -d -p 6821:6821 -v ./log  --label aliyun.logs.catalina=stdout  --label aliyun.logs.access=./log/.*.log  --label aliyun.logs.target= huarui_user_index  --name user user:test java -Dspring.profiles.active=test -jar /app/app.jar
+docker run -d -p 6821:6821 -v ./log  --label aliyun.logs.catalina=stdout  --label aliyun.logs.access=./log/.*.log    --name user user:test java -Dspring.profiles.active=test -jar /app/app.jar
+
+# --label aliyun.logs.target=huarui_user_index
