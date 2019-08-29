2e2b82226cf42445f2e0b6b0c7731628a981a27e
c568254965 <568254965@qq.com>
2019-08-29 17:14:17 +0800

    oo

--- run.sh
+++ run.sh
@@ -3,12 +3,12 @@
 git pull
 mvn clean package -Dmaven.test.skip=true
 
-docker rm -f user
-docker rmi -f user:test
-docker build -t user:test .
+docker rm -f huarui_user
+docker rmi -f c568254965/huarui_user:v1.0
+docker build -t c568254965/huarui_user:v1.0 .
 # docker run -d -p 6821:6821 --privileged=true  --name  user user:test java -Dspring.profiles.active=test -jar /app/app.jar
 
 
- docker run -d -p 6821:6821  -v /code/huarui_res/user/log-v:/log  --label aliyun.logs.catalina=stdout  --label aliyun.logs.access=/log/*.log    --name user user:test java -Dspring.profiles.active=test -jar /app/app.jar > user.log
+ docker run -d -p 6821:6821  -v /opt/my-code/user/log:/log  --label aliyun.logs.catalina=stdout  --label aliyun.logs.access=/log/*.log    --name huarui_user c568254965/huarui_user:v1.0  java -Dspring.profiles.active=test -jar /app/app.jar > user.log
 
 # --label aliyun.logs.target=huarui_user_index
