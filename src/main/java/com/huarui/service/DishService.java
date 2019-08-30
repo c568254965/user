d1a4f4c16a88d4a21cb6d3dbbbe917c49cb8fd86
c568254965 <568254965@qq.com>
2019-08-30 15:59:59 +0800

    ..

--- src/main/java/com/huarui/service/DishService.java
+++ src/main/java/com/huarui/service/DishService.java
@@ -5,6 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by sloan on 2019/7/15.
@@ -15,6 +17,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface DishService {
 
 
-    @GetMapping("/get/{id}")
+    /**
+     * 如果返回对象是map,可以用同属性名的对象接收。
+     */
+
+    @GetMapping("/csrr/dish/get/{id}")
     public String get(@PathVariable("id") Long id);
+//
+//    @RequestMapping(value = "uri",consumes = "application/json",method = RequestMethod.GET)
+//    public String get1(@RequestBody);
 }
