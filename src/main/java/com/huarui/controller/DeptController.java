6bd4adb4956b9bcca72395fad6a9f1e31e2c3758
c568254965 <568254965@qq.com>
2019-09-19 16:35:54 +0800

    ..

--- src/main/java/com/huarui/controller/DeptController.java
+++ src/main/java/com/huarui/controller/DeptController.java
@@ -1,7 +1,6 @@
 package com.huarui.controller;
 
 
-
 import com.huarui.config.MyRestTemplate;
 import com.huarui.service.AccountService;
 import com.huarui.service.DishService;
@@ -18,74 +17,64 @@ import java.util.List;
 
 @RestController
 @Slf4j
-public class DeptController
-{
+public class DeptController {
 
 
-	@Autowired
-	AccountService accountService;
+    @Autowired
+    AccountService accountService;
 
 
-	@Autowired
-	MyRestTemplate myRestTemplate;
+    @Autowired
+    MyRestTemplate myRestTemplate;
 
-	@Autowired
-	DishService dishService;
+    @Autowired
+    DishService dishService;
 
-	@Autowired
-	MqProducer mqProducer;
-
-
-
-	public static String DISH_URI = "http://HUARUIDISHES";
-	public static String DISH_PREFIX = "/csrr/dish";
-
-	@RequestMapping(value = "/get", method = RequestMethod.GET)
-	public String get()
-	{
-
-		log.info("this is get.....");
-		return accountService.findAccountById(1L).getName();
-
-	}
-
-	@RequestMapping(value = "/get1", method = RequestMethod.GET)
-	public String get1()
-	{
-
-		log.info("this is get1.....");
-		return accountService.findAccountById(1L).getName();
-
-	}
-
-	@RequestMapping(value = "/testDish", method = RequestMethod.GET)
-	public String testDish()
-	{
-
-		return myRestTemplate.initMyRestTemplate().getForObject(DISH_URI +DISH_PREFIX + "/get/1", String.class);
-	}
+    @Autowired
+    MqProducer mqProducer;
 
 
-	@RequestMapping(value = "/testDishFeign", method = RequestMethod.GET)
-	public String testDishFeigh()
-	{
+    public static String DISH_URI = "http://HUARUIDISHES";
+    public static String DISH_PREFIX = "/csrr/dish";
 
-		System.out.println("通过feign请求....");
-		return  dishService.get(1L)+",from feign";
-	}
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    public String get() {
+
+        log.info("this is get.....");
+        return accountService.findAccountById(1L).getName();
+
+    }
+
+    @RequestMapping(value = "/get1", method = RequestMethod.GET)
+    public String get1() {
+
+        log.info("this is get1.....");
+        return accountService.findAccountById(1L).getName();
+
+    }
+
+    @RequestMapping(value = "/testDish", method = RequestMethod.GET)
+    public String testDish() {
+        return myRestTemplate.initMyRestTemplate().getForObject(DISH_URI + DISH_PREFIX + "/get/1", String.class);
+    }
 
 
+    @RequestMapping(value = "/testDishFeign", method = RequestMethod.GET)
+    public String testDishFeigh() {
+
+        System.out.println("通过feign请求....");
+        return dishService.get(1L) + ",from feign";
+    }
 
 
-	@RequestMapping(value = "/orderDish/{userId}/{dishId}", method = RequestMethod.GET)
-	public String orderDish(@PathVariable("userId") Long userId,@PathVariable("dishId") Long dishId)
-	{
+    @RequestMapping(value = "/orderDish/{userId}/{dishId}", method = RequestMethod.GET)
+    public String orderDish(@PathVariable("userId") Long userId, @PathVariable("dishId") Long dishId) {
 
 
-		System.out.println("userId:"+userId+",dishId:"+dishId);
+        System.out.println("userId:" + userId + ",dishId:" + dishId);
 
-		mqProducer.orderDish(userId,dishId);
-		return "您点的菜已经交给后厨处理...请耐心等待";
-	}
+        mqProducer.orderDish(userId, dishId);
+        return "您点的菜已经交给后厨处理...请耐心等待";
+    }
 
 }
